a5a41ded66c0007849c4ef298040ba59ede8eb60
Saba Ahmad <sabaahmad895@gmail.com>
2023-03-18 15:14:38 +0500

    some changes in rental class

--- rental.rb
+++ rental.rb
@@ -1,7 +1,10 @@
 class Rental
-  attr_accessor :data
-
-  def initialize(_data)
-    @data = date
-  end
+    attr_accessor :date, :book, :person
+    def initialize(date, book, person)
+        @date=date
+        @book=book
+        @person=person
+        @book.rentals << self
+        @person.rentals << self
+    end
 end
--- student.rb
+++ student.rb
@@ -3,9 +3,9 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     super(name, age, parent_permission)
-    @classroom = classroom
+   
   end
 
   def play_hooky
