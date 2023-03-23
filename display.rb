5dfb1f84cfccd718d7ea24cc87bdcd5f54ccbe81
Saba Ahmad <sabaahmad895@gmail.com>
2023-03-23 23:55:54 +0500

    remove linter errors

--- display.rb
+++ display.rb
@@ -1,10 +1,10 @@
 def display_menu
-    puts 'Library App Menu:'
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List all rentals for a given person id'
-    puts '7. Quit the app'
-  end
+  puts 'Library App Menu:'
+  puts '1. List all books'
+  puts '2. List all people'
+  puts '3. Create a person'
+  puts '4. Create a book'
+  puts '5. Create a rental'
+  puts '6. List all rentals for a given person id'
+  puts '7. Quit the app'
+end
