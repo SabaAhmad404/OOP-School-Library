32cc062bc6fdb98f8a8a66a4c58225232aa76f10
Saba Ahmad <sabaahmad895@gmail.com>
2023-03-25 22:50:24 +0500

    some changes in main.rb

--- main.rb
+++ main.rb
@@ -34,17 +34,6 @@ class Main
   def quit
     puts 'Thank you for using the Library App. Goodbye!'
   end
-
-  # def display_menu
-  #   puts 'Library App Menu:'
-  #   puts '1. List all books'
-  #   puts '2. List all people'
-  #   puts '3. Create a person'
-  #   puts '4. Create a book'
-  #   puts '5. Create a rental'
-  #   puts '6. List all rentals for a given person id'
-  #   puts '7. Quit the app'
-  # end
 end
 
 main = Main.new
